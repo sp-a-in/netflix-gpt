74cec0e9031404b83a8561657a5f28db044a3f4e
Sparsh Jain <sparshjain325@gmail.com>
2025-03-16 14:52:17 +0530

    Routing

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
 import Browse from "./Browse"
 import Login from "./Login"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 
 const Body = () => {
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      Component: Login,
+    },
+    {
+      path: "/browse",
+      Component: Browse,
+    },
+  ]);
   return (
     <div>
-      <Login />
-      <Browse />
+      <RouterProvider router={router} />
     </div>
   )
 }
