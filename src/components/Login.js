583e28d9835207592cad496ae90356976b0a5fff
Sparsh Jain <sparshjain325@gmail.com>
2025-03-16 14:11:22 +0530

    SignUp handlind

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,29 @@
+import { useState } from "react";
+import NETFLIX_LOGO from "../utils/constants";
 import Header from "./Header";
 
 const Login = () => {
+    let [isSignInForm, setIsSignInForm] = useState(true)
+
+    let handleSignIn = () => {
+        setIsSignInForm(!isSignInForm);
+    }
+
     return (
         <div>
             <Header />
             <div className="absolute">
-                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/50fcc930-ba3f-4cae-9257-9f920e30a998/web/IN-en-20250310-TRIFECTA-perspective_739387a0-ff14-44ed-a5af-36e5aa4d236e_large.jpg" />
+                <img src={NETFLIX_LOGO} />
             </div>
                 <form className="absolute p-12 bg-black m-auto right-0 left-0 w-3/12 my-36 text-white opacity-85 rounded-lg">
-                    <h1 className="font-bold text-3xl py-4">Sign In</h1>
-                    <input type="text" placeholder="Email or mobile number" className="p-2 my-4 bg-gray-700 w-full"/>
-                    <input type="passwod" placeholder="Password" className="p-2 my-4 bg-gray-700 w-full" />
-                    <button className="bg-red-500 p-4 my-6 w-full"> Sign In</button>
+                    <h1 className="font-bold text-3xl py-4">Sign {isSignInForm ? "In" : "Up"}</h1>
+                    {
+                        !isSignInForm && <input type="text" placeholder="First Name" className="p-4 my-4 bg-gray-700 w-full rounded-md"/>
+                    }
+                    <input type="text" placeholder="Email or mobile number" className="p-4 my-4 bg-gray-700 w-full rounded-md"/>
+                    <input type="passwod" placeholder="Password" className="p-4 my-4 bg-gray-700 w-full rounded-md" />
+                    <button className="bg-red-500 p-2 my-2 w-full rounded-md font-medium"> Sign {isSignInForm ? "In" : "Up"}</button>
+                    <p  className="cursor-pointer my-4" onClick={handleSignIn}>{isSignInForm ? "New to Netflix ? Sign Up Now" : "Already a user ? Sign In Now"}</p>
                 </form>
         </div>
     )
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+let NETFLIX_LOGO = "https://assets.nflxext.com/ffe/siteui/vlv3/50fcc930-ba3f-4cae-9257-9f920e30a998/web/IN-en-20250310-TRIFECTA-perspective_739387a0-ff14-44ed-a5af-36e5aa4d236e_large.jpg";
+
+export default NETFLIX_LOGO;
