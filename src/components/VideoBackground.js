6f1862079c4f85c6f4c5c70b85779230502a8d73
Sparsh Jain <sparshjain325@gmail.com>
2025-03-23 04:32:11 +0530

    Created Custom Hook

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,28 +1,11 @@
-import { useEffect } from "react";
-import { OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../stores/slices/movieSlice";
+import { useSelector } from "react-redux";
+import useFetchTrailerVideo from "../hooks/useFetchTrailerVideo";
 
 let VideoBackground = ({recommendedMovieId}) => {
 
-    let dispatch = useDispatch();
+    useFetchTrailerVideo(recommendedMovieId);
+
     let trailerVideo = useSelector((state)=> state.movies.trailerVideo)
-    console.log('trailerVideo: ', trailerVideo);
-    
-    let fetchTrailerVideo = async () => {
-        let result = await fetch(`https://api.themoviedb.org/3/movie/${recommendedMovieId}/videos?language=en-US`, OPTIONS);
-        let allVideos = await result.json();
-        console.log('allVideos: ', allVideos.results);
-        let trailerVideos = allVideos.results.filter((videoObj)=> {
-            return videoObj.type == 'Trailer';
-        });
-
-        dispatch(addTrailerVideo(trailerVideos[2]))
-    }
-
-    useEffect(()=> {
-        fetchTrailerVideo();
-    }, [])
 
     if(!trailerVideo) {
         return <div>Loading..............</div>
---
+++ src/hooks/useFetchTrailerVideo.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../stores/slices/movieSlice";
+import { useEffect } from "react";
+import {OPTIONS} from "./../utils/constants";
+
+let useFetchTrailerVideo = (recommendedMovieId) => {
+    let dispatch = useDispatch();
+
+    
+    let fetchTrailerVideo = async () => {
+        let result = await fetch(`https://api.themoviedb.org/3/movie/${recommendedMovieId}/videos?language=en-US`, OPTIONS);
+        let allVideos = await result.json();
+        console.log('allVideos: ', allVideos.results);
+        let trailerVideos = allVideos.results.filter((videoObj)=> {
+            return videoObj.type == 'Trailer';
+        });
+
+        dispatch(addTrailerVideo(trailerVideos[0]))
+    }
+
+    useEffect(()=> {
+        fetchTrailerVideo();
+    }, [])
+}
+
+export default useFetchTrailerVideo;
