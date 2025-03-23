96aaf9cc3212d929bf5270c21727233e11181cd5
Sparsh Jain <sparshjain325@gmail.com>
2025-03-23 18:17:41 +0530

    scroll issue fix

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -8,7 +8,7 @@ const Browse = () => {
     useNowPlayingMovies();
 
     return (
-        <div>
+        <div className="w-screen">
             <Header />
             <MainContainer />
             <SecondaryContainer />
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,7 +2,7 @@ import MovieCard from "./movieCard";
 
 let MovieList = ({ listTitle, nowPlayingMovies }) => {
   return (
-    <div className="overflow-x-scroll w-screen p-6 no-scrollbar px-16">
+    <div className="overflow-x-scroll w-auto p-6 no-scrollbar px-16">
       <h1 className="text-3xl py-6 text-white">{listTitle}</h1>
       <div className="flex w-max">
         {nowPlayingMovies.results.map((movie) => (
