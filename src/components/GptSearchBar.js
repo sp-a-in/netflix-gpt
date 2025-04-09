0bf4f751eb51930aab3329778ad2e7ca2bd106a9
Sparsh Jain <sparshjain325@gmail.com>
2025-04-09 23:39:44 +0530

    Fix

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -19,7 +19,7 @@ let GptSearchBar = () => {
     }
 
     let handleGptSearch = async ()=> {
-        // console.log(searchText.current.value);
+        console.log(searchText.current.value);
         // let gptQuery = "Act as a movie recommendation system which takes keyword and suggest some movies for the query" + searchText.current.value + ". only give me names of 5 movies comma seprated "
         // console.log('gptQuery: ', gptQuery);
         // const completion = await openai.chat.completions.create({
